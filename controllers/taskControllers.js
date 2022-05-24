1af68ed594264080a328ea382199b819e94167d9
Revika Mukola <revikanikolaj@gmail.com>
2022-05-24 18:28:17 +0200

    Add index.js and taskControllers for /controllers

---
+++ controllers/taskControllers.js
@@ -0,0 +1,33 @@
+const { Todo } = require('./../models')
+
+module.exports.getTasks = (req, res) => {
+  const tasks = Todo.getTasks()
+  res.status(200).send(tasks)
+}
+
+module.exports.getTasksById = (req, res) => {
+  const { id } = req.params
+  const foundTasks = Todo.getTasksById(id)
+
+  foundTasks
+    ? res.status(200).send(foundTasks)
+    : res.status(200).send('Tasks Not Found')
+}
+
+module.exports.createTask = (req, res) => {
+  const { body } = req
+  const newTask = Todo.createTask(body)
+  res.status(201).send(newTask)
+}
+
+// module.exports.updateTask = (req, res) => {
+//   const { id } = req.params;
+//   const { body } = req;
+// };
+
+module.exports.removeTask = (req, res) => {
+  const { id } = req.params
+  Todo.removeTask(id)
+
+  res.status(200).end()
+}
